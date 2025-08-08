cba3948bf2f2fe1993ca10fe163f297747d55be4
Bishnu8568 <bishnucharandas8568@gmail.com>
2025-08-08 08:51:59 +0530

    update

--- C-game/Assets/Scripts/CarController.cs
+++ C-game/Assets/Scripts/CarController.cs
@@ -5,8 +5,10 @@ using UnityEngine;
 
 public class CarController : MonoBehaviour
 {
+   
     public float rotationSpeed, moveSpeed, boostMultiplier;
     public Transform cartransform;
+
 
     void Update()
     {
@@ -20,6 +22,27 @@ public class CarController : MonoBehaviour
         if (Math.Abs(verticalMovement) > 0.01f)
         {
             cartransform.Translate(0f, moveSpeed * verticalMovement * Time.deltaTime, 0f);
+        }
+    }
+
+    /*void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "RedHouse")
+        {
+            Debug.Log("I've hit a Red House");
+        }
+        if (collision.gameObject.tag == "BlueHouse")
+        {
+            Debug.Log("I've hit a Blue House");
+        }
+    }*/
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Boost")
+        {
+            moveSpeed = moveSpeed * boostMultiplier;
+            Destroy(other.gameObject);
         }
     }
 }
